15e1c3d18e42e263d3f0c85fd59f4933adb1b982
Owen Klan <owen.klan@equatetechnologies.com.au>
2021-09-05 08:19:03 +1000

    Introduce fields, fork table class before experimenting
    Going to "re-write" the BasicTable from scratch, but with
    fields being largely responsible for rendering a table cell.

--- src/texttab/fields.py
+++ src/texttab/fields.py
@@ -1,5 +1,9 @@
 from const import FG_COLOURS, BG_COLOURS
 
+DEFAULT_FG = "white"
+DEFAULT_BG = "black"
+
+
 class Field(object):
     """
     Fields encapsulate advanced features for table cells, like
@@ -23,4 +27,14 @@ class Field(object):
     def __str__(self):
         prefix = FG_COLOURS[self._fg] + BG_COLOURS[self._bg]
         suffix = "\033[0m"
-        return prefix + value + suffix
+        return prefix + str(self.value) + suffix
+
+    @classmethod
+    def from_column(cls, column, value):
+        field_fg = DEFAULT_FG
+        field_bg = DEFAULT_BG
+        if "fg" in column:
+            field_fg = column["fg"]
+        if "bg" in column:
+            field_bg = column["bg"]
+        return Field(value, fg=field_fg, bg=field_bg)
