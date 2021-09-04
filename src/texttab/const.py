cddd012326e560e73023985f58df78af0b933604
Owen Klan <owen.klan@equatetechnologies.com.au>
2021-09-05 07:43:21 +1000

    Add beginning of "fields" and colour-awareness
    This is intended to finally bring coloured cells to the tables
    that don't get screwed up length calculations due to ANSI
    escape sequences, which aren't visible, but do count towards
    string length.

--- src/texttab/const.py
+++ src/texttab/const.py
@@ -79,3 +79,26 @@ border_symbols = {
         "HBAR": u"\u2500",
     },
 }
+
+# British spelling, deal with it.
+FG_COLOURS = {
+    "black": "\033[30m",
+    "red": "\033[31m",
+    "green": "\033[32m",
+    "yellow": "\033[33m",
+    "blue": "\033[34m",
+    "magenta": "\033[35m",
+    "cyan": "\033[36m",
+    "white": "\033[37m"
+}
+
+BG_COLOURS = {
+    "black": "\033[40m",
+    "red": "\033[41m",
+    "green": "\033[42m",
+    "yellow": "\033[43m",
+    "blue": "\033[44m",
+    "magenta": "\033[45m",
+    "cyan": "\033[46m",
+    "white": "\033[47m"
+}
