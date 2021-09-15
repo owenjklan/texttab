01aa78a219d7676f2ba43a0b1e7af77fb405dbb0
Owen Klan <owen.klan@equatetechnologies.com.au>
2021-09-15 19:21:31 +1000

    Add beginnings of files for pypi publishing
    Chose MIT License

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-#!/usr/bin/env python
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils import setup
-
+from distutils.core import setup
 setup(
-    name="texttab",
-    packages=["texttab"],
-    package_dir={"": "src"},
-    version="0.1.1",
+  name = 'texttab',         # How you named your package folder (MyLib)
+  packages = ['texttab'],   # Chose the same as "name"
+  version = '0.5.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Create ASCII tables that are flexible. Colours, various border styles and custom column formatters are some of the features.',   # Give a short description about your library
+  author = 'Owen Klan',                   # Type in your name
+  author_email = 'owen.j.klan@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/owenjklan/texttab',   # Provide either the link to your github or to your website
+  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['command-line', 'ascii', 'tables', 'formatters', 'extensible'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+      ],
+  classifiers=[
+    'Development Status :: 3 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    # Because we heavily use f-strings, a python 3.6 feature
+    'Programming Language :: Python :: 3.6',
+  ],
 )
-
