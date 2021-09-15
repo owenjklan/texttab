92a639b4515baad43c9cba999bf159077a7de753
Owen Klan <owen.klan@equatetechnologies.com.au>
2021-09-15 19:29:20 +1000

    Adjust directories layout for packaging
    I think this is how I need to lay things out
    for a Pip-installable package... maybe???

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Owen Klan',                   # Type in your name
   author_email = 'owen.j.klan@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/owenjklan/texttab',   # Provide either the link to your github or to your website
-  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/owenjklan/texttab/archive/refs/tags/v0.5.0.tar.gz',    # I explain this later on
   keywords = ['command-line', 'ascii', 'tables', 'formatters', 'extensible'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
